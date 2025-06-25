fce769b69ed7ec7bd25c2d03f71452f23fe9ed42
SAljar99 <saudaljar2002@gmail.com>
2025-06-25 09:28:44 +0300

    booking model made

---
+++ models/Booking.js
@@ -0,0 +1,14 @@
+const { Schema } = require('mongoose');
+
+const bookingSchema = new Schema(
+  {
+    userID: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    flatID: { type: Schema.Types.ObjectId, ref: 'Flat', required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
+    isBooked: { type: Boolean, default: true }
+  },
+  { timestamps: true }
+);
+
+module.exports =  bookingSchema;
