8e5b57e844bae71723828cd0f77c63e52bc52e37
SAljar99 <saudaljar2002@gmail.com>
2025-06-25 09:26:18 +0300

    flat model made

---
+++ models/Flat.js
@@ -0,0 +1,15 @@
+const { Schema } = require("mongoose")
+
+const flatSchema = new Schema(
+  {
+ number: { type: Number, required: true },
+    type: { type: String, required: true },
+    isRented: { type: Boolean, default: false },
+    price: { type: Number, required: true },
+    branchId: { type: Schema.Types.ObjectId, ref: 'HotelBranch', required: true },
+    flatImages: [{ type: String }]
+  },
+  { timestamps: true }
+)
+
+module.exports = flatSchema
--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
-const { Schema } = require('mongoose')
+const { Schema } = require("mongoose")
 
 const userSchema = new Schema(
   {
     fullName: { type: String, required: true },
-     cprFront: { type: String, required: true },
+    cprFront: { type: String, required: true },
     cprBack: { type: String, required: true },
     phone: { type: Number, required: true },
     email: { type: String, required: true },
-    passwordDigest: { type: String, required: true }
+    passwordDigest: { type: String, required: true },
   },
   { timestamps: true }
 )
