01fae0c60b39fc1828be865db41d88c468ea9f0e
SAljar99 <saudaljar2002@gmail.com>
2025-06-30 11:59:32 +0300

    added a link to dashboard

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -7,6 +7,7 @@ const Nav = ({ user, handleLogOut }) => {
     userOptions = (
       <nav>
         <h3>Welcome {user.fullName}!</h3>
+          <Link to="/dashboard">Dashboard</Link>
         <Link onClick={handleLogOut} to="/">
           Sign Out
         </Link>
